edccbab19d8759413cb9014b686f9f882aa139e1
Benny623 <bmilner88@gmail.com>
2022-07-01 20:36:09 -0700

    setup the main flask app

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from app.routes import home, dashboard
+
+def create_app(test_config=None):
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_KEY='super_secret_key'
+    )
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello World'
+
+    app.register_blueprint(home)
+    app.register_blueprint(dashboard)
+
+    return app

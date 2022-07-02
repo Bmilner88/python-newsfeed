1af4a0c673e1806ee0aa4ae6fde81a1d8fb6fc6b
Benny623 <bmilner88@gmail.com>
2022-07-01 21:16:15 -0700

    save progress

---
+++ seeds.py
@@ -0,0 +1,19 @@
+from app.models import User
+from app.db import Session, Base, engine
+
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+db = Session()
+
+db.add_all([
+    User(username='alesmonde0', email='nwestnedge0@cbc.ca', password='password123'),
+    User(username='jwilloughway1', email='rmebes1@sogou.com', password='password123'),
+    User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
+    User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
+    User(username='djiri4', email='gmidgley4@weather.com', password='password123')
+])
+
+db.commit()
+
+db.close()

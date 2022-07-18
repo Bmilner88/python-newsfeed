cac4aea59ec3025e01366024e42206fe7fbd5cfa
Benny623 <bmilner88@gmail.com>
2022-07-18 16:35:08 -0700

    finalized the homepage views

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from app.routes import home, dashboard
+from app.db import init_db
+from app.utils import filters
 
 def create_app(test_config=None):
     # set up app config
@@ -9,12 +11,15 @@ def create_app(test_config=None):
         SECRET_KEY='super_secret_key'
     )
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello World!'
+    # filter registration
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
 
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+
+    init_db(app)
 
     return app
